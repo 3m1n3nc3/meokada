d80efcafeb5a40adecec84944877ae7101bb3701
3m1n3nc3 <mygames.ng@gmail.com>
2020-05-23 22:58:43 +0100

    Changed Favicon on admin panel

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -133,7 +133,6 @@ if (empty($page_content)) {
         </title>
         
         <link rel="shortcut icon" type="image/png" href="<?php echo($config['site_url']); ?>/media/img/icon.<?php echo($config['favicon_extension']); ?>"/> 
-
         
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css"> 
